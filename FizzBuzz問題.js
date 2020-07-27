e6c97a6dc6af1e421721a28923d37af451d1fa2f
Yoshino666 <68672175+Yoshino666@users.noreply.github.com>
2020-07-27 23:22:26 +0900

    Add files via upload

---
+++ FizzBuzz問題.js
@@ -0,0 +1,13 @@
+//1から20までで、3の倍数の時Fizz、5の倍数の時Buzz、3と5両方の倍数の時FizzBuzz
+for (var i = 0 ; i < 20 ; i++){
+    console.log(i)
+}
+if(i%3 === 0){
+    console .log('Fizz')
+}
+else if(i%5 === 0){
+    console.log('Buzz')
+}
+else((i%3 === 0)&&(i%5 === 0)){
+    console.log('FizzBuzz')
+}

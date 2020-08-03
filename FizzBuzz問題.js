be955a01935cf941ebd9df2c3c9d35b4789c2b66
Yoshino666 <68672175+Yoshino666@users.noreply.github.com>
2020-08-03 14:22:00 +0900

    Update FizzBuzz問題.js

--- FizzBuzz問題.js
+++ FizzBuzz問題.js
@@ -1,6 +1,6 @@
 //1から20まで、3の倍数の時Fizz、5の倍数の時Buzz、3と5両方の倍数の時FizzBuzz
 for (var i = 0 ; i < 20 ; i++){
-　　if(i%3 === 0)&&(i%5 === 0){
+　　if(i%3 === 0 && i%5 === 0){
     　　console.log('FizzBuzz')
 }
 　　　else if(i%3 === 0){
